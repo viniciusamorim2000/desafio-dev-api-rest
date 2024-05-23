bf57dbaa196af4a73b5c94848678fffbc72c2988
viniciusamorim2000 <amorimhavoc@gmail.com>
2024-05-23 16:36:59 -0300

    incluindo operacao deposito

--- src/main/java/com/dock/dock/controller/ContaController.java
+++ src/main/java/com/dock/dock/controller/ContaController.java
@@ -1,16 +1,12 @@
 package com.dock.dock.controller;
 
 import com.dock.dock.controller.dto.conta.ContaResponseDTO;
-import com.dock.dock.controller.dto.extrato.ExtratoResponseDTO;
 import com.dock.dock.service.ContaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/contas")
@@ -33,7 +29,7 @@ public class ContaController {
     public ResponseEntity<ContaResponseDTO> consultarConta(@PathVariable String cpf) {
         return ResponseEntity.status(HttpStatus.OK).
                 body(mapper.map(contaService.
-                        consultaConta(cpf), ContaResponseDTO.class));
+                        consultaContaPorCpf(cpf), ContaResponseDTO.class));
     }
 
 
--- src/main/java/com/dock/dock/service/impl/TransacaoServiceImpl.java
+++ src/main/java/com/dock/dock/service/impl/TransacaoServiceImpl.java
@@ -1,12 +1,28 @@
 package com.dock.dock.service.impl;
 
+import com.dock.dock.domain.entity.ContaEntity;
 import com.dock.dock.domain.entity.TransacaoEntity;
+import com.dock.dock.domain.entity.enums.TipoTransacao;
 import com.dock.dock.domain.model.ExtratoModel;
+import com.dock.dock.repository.TransacaoRepository;
 import com.dock.dock.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Service
 public class TransacaoServiceImpl implements TransacaoService {
+
+    @Autowired
+    private TransacaoRepository transacaoRepository;
+
+    @Autowired
+    private ContaServiceImpl contaService;
+
+    private static final BigDecimal LIMITE_DIARIO_SAQUE = new BigDecimal("2000");
+
     @Override
     public ExtratoModel consultarExtrato(String cpf, LocalDateTime dataIncio, LocalDateTime dataFim) {
         return null;
@@ -14,7 +30,38 @@ public class TransacaoServiceImpl implements TransacaoService {
 
     @Override
     public TransacaoEntity efetuarTransacao(TransacaoEntity transacaoEntity) {
+        ContaEntity contaEntity = buscarConta(transacaoEntity.getNumeroConta().getNumero());
+        if (transacaoEntity.getTipoTransacao().equals("DEPOSITO")){
+            return depositar(contaEntity, transacaoEntity.getValorTransacao());
+        } else {
+            return sacar();
+        }
+
+    }
+
+    private TransacaoEntity sacar() {
         return null;
+    }
+
+
+    public TransacaoEntity depositar(ContaEntity conta, BigDecimal valor) {
+            conta.setSaldo(conta.getSaldo().add(valor));
+            contaService.salvarConta(conta);
+            TransacaoEntity transacaoEntity = new TransacaoEntity();
+            transacaoEntity.setTipoTransacao(TipoTransacao.DEPOSITO);
+            transacaoEntity.setValorTransacao(valor);
+            transacaoEntity.setNumeroConta(conta);
+            transacaoEntity.setDataHoraTransacao(LocalDateTime.now());
+            return salvarTransacao(transacaoEntity);
+
+    }
+
+    public TransacaoEntity salvarTransacao(TransacaoEntity transacaoEntity){
+        return transacaoRepository.save(transacaoEntity);
+    }
+
+    private ContaEntity buscarConta(Integer numeroConta) {
+        return contaService.consultaContaPorNumeroConta(numeroConta);
     }
 
 
