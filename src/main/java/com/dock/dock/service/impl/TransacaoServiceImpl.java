c22f1371ef8bd30ed4555f5dd3a5b11c808630b9
viniciusamorim2000 <amorimhavoc@gmail.com>
2024-05-23 22:41:26 -0300

    incluindo operações de saque

--- src/main/java/com/dock/dock/service/impl/TransacaoServiceImpl.java
+++ src/main/java/com/dock/dock/service/impl/TransacaoServiceImpl.java
@@ -10,7 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransacaoServiceImpl implements TransacaoService {
@@ -31,32 +35,75 @@ public class TransacaoServiceImpl implements TransacaoService {
     @Override
     public TransacaoEntity efetuarTransacao(TransacaoEntity transacaoEntity) {
         ContaEntity contaEntity = buscarConta(transacaoEntity.getNumeroConta().getNumero());
-        if (transacaoEntity.getTipoTransacao().equals(TipoTransacao.DEPOSITO)){
-            return depositar(contaEntity, transacaoEntity.getValorTransacao());
+        switch (transacaoEntity.getTipoTransacao()) {
+            case DEPOSITO:
+                return depositar(contaEntity, transacaoEntity.getValorTransacao());
+            case SAQUE:
+                return sacar(contaEntity, transacaoEntity.getValorTransacao());
+            default:
+                throw new IllegalArgumentException("Tipo transação inválido.");
+        }
+    }
+
+
+    private TransacaoEntity sacar(ContaEntity conta, BigDecimal valor) {
+        if (conta.getSaldo().compareTo(valor) > 0) {
+            BigDecimal valorSacadoDia = verificaTransacoes(todasTransacoesPorContaDataTransacao(conta));
+
+            if (LIMITE_DIARIO_SAQUE.compareTo(valorSacadoDia.add(valor)) > 0) {
+                conta.setSaldo(conta.getSaldo().subtract(valor));
+                contaService.salvarConta(conta);
+                TransacaoEntity transacaoEntity = new TransacaoEntity();
+                transacaoEntity.setTipoTransacao(TipoTransacao.SAQUE);
+                transacaoEntity.setValorTransacao(valor);
+                transacaoEntity.setNumeroConta(conta);
+                transacaoEntity.setDataHoraTransacao(LocalDateTime.now());
+                return salvarTransacao(transacaoEntity);
+            } else {
+                throw new IllegalArgumentException("Limite diário excedido.");
+            }
         } else {
-            return sacar();
+            throw new IllegalArgumentException("Saldo insuficiente.");
         }
 
     }
 
-    private TransacaoEntity sacar() {
-        return null;
+//    private BigDecimal verificaTransacoes(List<TransacaoEntity> transacoes) {
+//        return transacoes.stream()
+//                .filter(tipoTransacao -> tipoTransacao.getTipoTransacao().equals(TipoTransacao.SAQUE))
+//                .filter(dataTransacao -> dataTransacao.getDataHoraTransacao().equals(LocalDate.now()))
+//                .map(TransacaoEntity::getValorTransacao)
+//                .collect(Collectors.reducing(BigDecimal.ZERO, BigDecimal::add));
+//    }
+
+    private BigDecimal verificaTransacoes(List<TransacaoEntity> transacoes) {
+
+        BigDecimal collect = transacoes.stream()
+                .filter(tipoTransacao -> tipoTransacao.getTipoTransacao().equals(TipoTransacao.SAQUE))
+                .filter(dataTransacao -> dataTransacao.getDataHoraTransacao().toLocalDate().equals(LocalDate.now()))
+                .map(TransacaoEntity::getValorTransacao)
+                .collect(Collectors.reducing(BigDecimal.ZERO, BigDecimal::add));
+        return collect;
+    }
+
+    public List<TransacaoEntity> todasTransacoesPorContaDataTransacao(ContaEntity contaEntity) {
+        return transacaoRepository.findByNumeroConta(contaEntity);
     }
 
 
     public TransacaoEntity depositar(ContaEntity conta, BigDecimal valor) {
-            conta.setSaldo(conta.getSaldo().add(valor));
-            contaService.salvarConta(conta);
-            TransacaoEntity transacaoEntity = new TransacaoEntity();
-            transacaoEntity.setTipoTransacao(TipoTransacao.DEPOSITO);
-            transacaoEntity.setValorTransacao(valor);
-            transacaoEntity.setNumeroConta(conta);
-            transacaoEntity.setDataHoraTransacao(LocalDateTime.now());
-            return salvarTransacao(transacaoEntity);
+        conta.setSaldo(conta.getSaldo().add(valor));
+        contaService.salvarConta(conta);
+        TransacaoEntity transacaoEntity = new TransacaoEntity();
+        transacaoEntity.setTipoTransacao(TipoTransacao.DEPOSITO);
+        transacaoEntity.setValorTransacao(valor);
+        transacaoEntity.setNumeroConta(conta);
+        transacaoEntity.setDataHoraTransacao(LocalDateTime.now());
+        return salvarTransacao(transacaoEntity);
 
     }
 
-    public TransacaoEntity salvarTransacao(TransacaoEntity transacaoEntity){
+    public TransacaoEntity salvarTransacao(TransacaoEntity transacaoEntity) {
         return transacaoRepository.save(transacaoEntity);
     }
 
