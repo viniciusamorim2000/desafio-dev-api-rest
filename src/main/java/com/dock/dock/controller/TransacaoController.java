c483e04c26f64b3851f24628e99d4f9ad8815908
viniciusamorim2000 <amorimhavoc@gmail.com>
2024-05-25 15:40:32 -0300

    finalizando swagger

--- src/main/java/com/dock/dock/controller/TransacaoController.java
+++ src/main/java/com/dock/dock/controller/TransacaoController.java
@@ -4,6 +4,9 @@ import com.dock.dock.controller.dto.transacao.TransacaoRequestDTO;
 import com.dock.dock.controller.dto.transacao.TransacaoResponseDTO;
 import com.dock.dock.domain.entity.TransacaoEntity;
 import com.dock.dock.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/transacao")
+@Tag(name = "Transações", description = "API cadastro de transações.")
 public class TransacaoController {
 
     @Autowired
@@ -25,9 +29,13 @@ public class TransacaoController {
     private TransacaoService transacaoService;
 
     @GetMapping("/extrato/{numeroConta}")
-    public ResponseEntity<List<TransacaoResponseDTO>> consultarExtrato(@PathVariable Integer numeroConta,
-                                                                     @RequestParam LocalDate dataInicio,
-                                                                     @RequestParam LocalDate dataFim) {
+    @Operation(summary = "Consulta extrato de portador por número da conta entre um período e outro.", tags = "Transações")
+    public ResponseEntity<List<TransacaoResponseDTO>> consultarExtrato(@Parameter(description = "Número da conta", example = "275824")
+                                                                           @PathVariable Integer numeroConta,
+                                                                       @Parameter(description = "Data inicial", example = "2024-01-01")
+                                                                       @RequestParam LocalDate dataInicio,
+                                                                       @Parameter(description = "Data fim", example = "2024-04-29")
+                                                                           @RequestParam LocalDate dataFim) {
         List<TransacaoEntity> transacoes = transacaoService.consultarExtrato(numeroConta, dataInicio, dataFim);
         List<TransacaoResponseDTO> dtoList = transacoes.stream()
                 .map(transacao -> mapper.map(transacao, TransacaoResponseDTO.class))
@@ -36,6 +44,7 @@ public class TransacaoController {
     }
 
     @PostMapping
+    @Operation(summary = "Efetua transações de saque ou deposito.", tags = "Transações")
     public ResponseEntity<TransacaoResponseDTO> efetuarTransacao(@RequestBody TransacaoRequestDTO transacaoRequestDTO){
         return ResponseEntity.status(HttpStatus.CREATED).
                 body(mapper.map(transacaoService.
